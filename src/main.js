d183cefaf59ae01b3b6a8593faee6b7ca9d124f9
Ryan <ryan@thecpafirm.com>
2021-03-06 22:45:47 -0700

    good greif charlie brown

--- src/main.js
+++ src/main.js
@@ -1,2 +1,21 @@
 const connect = "https://hacker-news.firebaseio.com/v0/"
 
+console.log('Hello Console World!')
+
+const getTopArt = () =>{
+ 
+    let getArtID = new Promise( (resolve, reject) =>{
+        fetch(`${connect}topstories.json?print=pretty`, {
+            method: 'GET'
+        }).then( value => {
+            resolve(value.json())
+        })
+    })
+
+    let x = Promise.all([getArtID])
+
+    console.log(getArtID)
+    console.log(x)
+}
+
+window.onload = getTopArt()
