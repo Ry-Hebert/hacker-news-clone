4c1a18226d85eaa8bccbbd3aef214a3be3b0032a
Ryan <ryan@thecpafirm.com>
2021-03-10 17:49:13 -0700

    trying to get promise all to work

--- src/main.js
+++ src/main.js
@@ -3,19 +3,37 @@ const connect = "https://hacker-news.firebaseio.com/v0/"
 console.log('Hello Console World!')
 
 const getTopArt = () =>{
- 
+    
+    let topStoriesID
     let getArtID = new Promise( (resolve, reject) =>{
         fetch(`${connect}topstories.json?print=pretty`, {
             method: 'GET'
         }).then( value => {
             resolve(value.json())
         })
+    }).then( value => topStoriesID = value)
+    .then(valArray =>{
+        Promise.all(valArray).then( pleaseWork =>{
+            let fetchedData = pleaseWork.map(x =>{
+                // console.log(x)
+                fetchHNArt(x)
+            })
+            Promise.all(fetchedData).then( x =>{
+                console.log(x)
+            })
+        })
+
     })
 
-    let x = Promise.all([getArtID])
-
-    console.log(getArtID)
-    console.log(x)
+    
 }
 
 window.onload = getTopArt()
+
+const fetchHNArt = (idNum) =>{
+    fetch(`${connect}item/${idNum}.json?print=pretty`, {
+        method: 'GET'
+    }).then( value => {
+        return value.json()
+    })
+}
